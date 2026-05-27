f407fe7c9bd6950f8172a22d50457b693624c182
Jigar <sonanijigar797@gmail.com>
2026-05-27 11:10:41 +0530

    add 3D MacBook viewer with GSAP animations and model controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Hero from "./components/hero"
 import NavBar from "./components/NavBar"
+import ProductViewer from "./components/Productviewer"
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all"
+
+gsap.registerPlugin(ScrollTrigger)
 
 const App = () => {
     return (
         <main>
             <NavBar />
             <Hero />
+            <ProductViewer />
         </main>
     )
 }
