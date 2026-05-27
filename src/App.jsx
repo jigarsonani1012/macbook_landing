acef2aebeffd03f1aef0ef6d6c60ee5250eed0bd
Jigar <sonanijigar797@gmail.com>
2026-05-27 15:25:47 +0530

    add Highlights and Footer components; update App to include new sections

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import { ScrollTrigger } from "gsap/all"
 import Showcase from "./components/Showcase";
 import Performance from "./components/Performance";
 import Features from "./components/Features";
+import Highlights from "./components/Highlights";
+import Footer from "./components/Footer";
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -18,6 +20,8 @@ const App = () => {
             <Showcase />
             <Performance />
             <Features />
+            <Highlights />
+            <Footer />
         </main>
     )
 }
